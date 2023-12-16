c21b5146c17284c7a434444469de13f0a223d364
SysAnalysis <149229111+SysAnalysis@users.noreply.github.com>
2023-12-16 12:21:13 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// Function to calculate the summation of two numbers
+double sum(double a, double b) {
+    return a + b;
+}
+
+
+
+
+
+int main() {
+    // Get user input for two numbers
+    double num1, num2;
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    // Perform operations and print results
+    std::cout << "Operation: " << sum(num1, num2) << std::endl;
+   
+    return 0;
+}
